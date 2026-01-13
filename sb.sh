211fd59500c5d08a2621ad0e37314a38e558b8ae
hooghub <goookuooog@gmail.com>
2026-01-13 06:47:20 -0800

    Update sb.sh

--- sb.sh
+++ sb.sh
@@ -119,12 +119,23 @@ if [[ "$MODE" == "1" ]]; then
         cp "$LE_KEY_PATH" "$CERT_DIR/privkey.pem"
         chmod 644 "$CERT_DIR"/*.pem
     else
-        echo ">>> 申请新的 Let's Encrypt TLS 证书"
-        /root/.acme.sh/acme.sh --issue -d "$DOMAIN" --standalone --keylength ec-256 --force
-        /root/.acme.sh/acme.sh --install-cert -d "$DOMAIN" --ecc \
-            --key-file "$CERT_DIR/privkey.pem" \
-            --fullchain-file "$CERT_DIR/fullchain.pem" --force
+            echo ">>> 申请新的 Let's Encrypt TLS 证书"
+    
+    # 如果 VPS IPv4 匹配域名 A 记录
+    if [[ $USE_IPv4 -eq 1 ]]; then
+        /root/.acme.sh/acme.sh --issue -d "$DOMAIN" --standalone --listen-v4 --keylength ec-256 --force
+    # 如果 VPS IPv6 匹配域名 AAAA 记录
+    elif [[ $USE_IPv6 -eq 1 ]]; then
+        /root/.acme.sh/acme.sh --issue -d "$DOMAIN" --standalone --listen-v6 --keylength ec-256 --force
     fi
+    
+    # 安装证书到 sing-box
+    /root/.acme.sh/acme.sh --install-cert -d "$DOMAIN" --ecc \
+        --key-file "$CERT_DIR/privkey.pem" \
+        --fullchain-file "$CERT_DIR/fullchain.pem" --force
+    chmod 644 "$CERT_DIR"/*.pem
+    echo "[✔] TLS 证书申请完成"
+        fi
 else
     # --------- 自签固定域名模式 ---------
     DOMAIN="www.epple.com"
